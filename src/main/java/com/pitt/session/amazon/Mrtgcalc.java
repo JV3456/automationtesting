867b38ebdd596326a7045d92185587d0f686123a
JV3456 <57292593+JV3456@users.noreply.github.com>
2019-11-02 10:51:56 -0400

    Add files via upload

---
+++ src/main/java/com/pitt/session/amazon/Mrtgcalc.java
@@ -0,0 +1,13 @@
+package com.pitt.session.amazon;
+
+public class Mrtgcalc {
+ 
+	@FindBy(id="homeval")	
+	WebElement homevaluetext;
+	@FindBy(id="")	
+	WebElement downpymtnttext;
+	@FindBy(id="")	
+	WebElement loanamnttext;
+	@FindBy(id="")	
+	WebElement intrstratetext;
+}
